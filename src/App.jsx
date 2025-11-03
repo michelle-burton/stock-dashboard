e98a3651fabab40e76564e9494c0265209816225
Michelle Burton <msmichelleburton@yahoo.com>
2025-11-03 07:16:05 -0600

    pulling in stock data

--- src/App.jsx
+++ src/App.jsx
@@ -2,43 +2,38 @@ import { useState } from 'react'
 import './index.css'
 
 function App() {
+    const [results, setResults] = useState(null);
+    const apiKey = import.meta.env.VITE_FINNHUB_API_KEY;
 
-    function handleClick() {
-        console.log("in click")
+    async function searchSymbol(symbol) {
+        console.log(import.meta.env.VITE_FINNHUB_API_KEY);
+        try {
+            const response = await fetch(
+                `https://finnhub.io/api/v1/search?q=${symbol}&token=${apiKey}`
+            )
+            const data = await response.json();
+            setResults(data || "No Data");
+        } catch (error) {
+            console.error("Error fetching symbol:", error)
+       }
     }
+
 
   return (
       <>
           <h1>Stock Dashboard</h1>
-          <button
-              onClick={handleClick}
+          {/* <button
+              onClick={getStockPrice}
               className="px-4 py-2 bg-blue-600 text-white rounded-lg hover:bg-blue-700"
-          >Fetch Data</button>
-{/* 
-          <div className="grid grid-cols-3 gap-4">
-  <div>Item 1</div>
-  <div>Item 2</div>
-  <div>Item 3</div>
-  <div>Item 4</div>
-          </div>
-           
-          <div>
-              <div className="flex items-center gap-1.5 sm:flex-col"><div className="bg-sky-100"></div><p className="text-center font-mono text-xs text-gray-600 dark:text-gray-400">100</p></div>
-  <div className="bg-sky-50">x</div>
-  <div className="bg-sky-100"></div>
-  <div className="bg-sky-200"></div>
-  <div className="bg-sky-300"></div>
-  <div className="bg-sky-400"></div>
-  <div className="bg-sky-500"></div>
-  <div className="bg-sky-600"></div>
-  <div className="bg-sky-700"></div>
-  <div className="bg-sky-800"></div>
-  <div className="bg-sky-900"></div>
-  <div className="bg-sky-950"></div>
-          </div>
+          >Get Stock Price</button> */}
+           <button onClick={() => searchSymbol("AAPL")}>Search AAPL</button>
           
-<div className="w-6 h-6 bg-red-500">xxx</div> */}
-
+          {results && 
+            <div>
+                <h3>Results:</h3>
+                <pre>{JSON.stringify(results, null, 2)}</pre>
+            </div>
+           }
 
     </>
   )
